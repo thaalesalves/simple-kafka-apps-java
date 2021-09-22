b1b631636ca22198c2743312f6b63b2bca909705
Thales Alves <thales@thalesalv.es>
2021-09-22 14:22:22 -0300

    initial commit

---
+++ src/main/java/es/thalesalv/kafkstreams/KafkstreamsApplication.java
@@ -0,0 +1,13 @@
+package es.thalesalv.kafkstreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkstreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkstreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/thalesalv/kafkstreams/KafkstreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.thalesalv.kafkstreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkstreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
