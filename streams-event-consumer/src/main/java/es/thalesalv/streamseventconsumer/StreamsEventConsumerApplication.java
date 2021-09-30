66baf41f3ff8e4a3be953ce55bc770e0bfe9a741
Thales Alves <thales@thalesalv.es>
2021-09-30 07:59:28 -0300

    created streams event consumer for proof of concept

---
+++ streams-event-consumer/src/main/java/es/thalesalv/streamseventconsumer/StreamsEventConsumerApplication.java
@@ -0,0 +1,13 @@
+package es.thalesalv.streamseventconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamsEventConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsEventConsumerApplication.class, args);
+	}
+
+}
