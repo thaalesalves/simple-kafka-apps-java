74bdf5a3937644792868bed6952f0fa997f56d0f
Thales Alves <thales@thalesalv.es>
2021-09-30 13:12:07 -0300

    removed unused producers

--- streams-event-conprod/src/main/java/es/thalesalv/streamsconprod/adapters/event/producer/ProducerService.java
+++ streams-event-conprod/src/main/java/es/thalesalv/streamsconprod/adapters/event/producer/ProducerService.java
@@ -2,10 +2,7 @@ package es.thalesalv.streamsconprod.adapters.event.producer;
 
 import org.apache.avro.specific.SpecificRecord;
 
-import es.thalesalv.avro.MagazineSchema;
-
 public interface ProducerService {
 
-    void produce(MagazineSchema record);
     void produce(SpecificRecord record, String topic);
 }
--- streams-event-conprod/src/main/java/es/thalesalv/streamsconprod/adapters/event/producer/ProducerServiceImpl.java
+++ streams-event-conprod/src/main/java/es/thalesalv/streamsconprod/adapters/event/producer/ProducerServiceImpl.java
@@ -2,11 +2,9 @@ package es.thalesalv.streamsconprod.adapters.event.producer;
 
 import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import es.thalesalv.avro.MagazineSchema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,25 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ProducerServiceImpl implements ProducerService {
 
-    @Value("${app.kafka.producer.topics.magazines}")
-    private String magazinesTopic;
-
     private final KafkaTemplate<String, SpecificRecord> kafkaProducer;
 
     @Override
-    public void produce(MagazineSchema record) {
-        log.debug("Start of event production method");
-        ProducerRecord<String, SpecificRecord> producerRecord = new ProducerRecord<>(magazinesTopic, record);
-        kafkaProducer.send(producerRecord).addCallback(success -> {
-            log.info("Event produced successfully -> {}", record);
-        }, failure -> {
-            log.error("Exception caught while producing event -> {}", failure);
-            throw new RuntimeException("Exception caught while producing event.", failure);
-        });
-    }
-
-    @Override
     public void produce(SpecificRecord record, String topic) {
+    
         log.debug("Start of event production method");
         ProducerRecord<String, SpecificRecord> producerRecord = new ProducerRecord<>(topic, record);
         kafkaProducer.send(producerRecord).addCallback(success -> {
